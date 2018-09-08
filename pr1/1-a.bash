d79ff4a08528342ce97452d623a623563056a626
AdamVanScyoc <Avanscy@clemson.edu>
2018-09-08 16:20:08 -0400

    Fix parse_extensions 'return value' for fs1

--- pr1/1-a.bash
+++ pr1/1-a.bash
@@ -13,7 +13,6 @@ function parse_extensions {
 	if [ "$1" = "fs1" ]; then
 		# If in fs1/, all files without an extension in extensions/ contain file listings.
 		cd fs1/extensions
-		pwd
 		#for file  in `find . -type f ! -name "*.txt" ! -name "*.sh" ! -name "*.pl" ! -name "*.py" ! -name "*.bash"`; do
 		files=$(find "$PWD" -type f ! -name "*.txt" ! -name "*.sh" ! -name "*.pl" ! -name "*.py" ! -name "*.bash" ! -name "*.swo" ! -name "*.swp" ! -name "*.zip" -printf "%P ")
 
@@ -22,39 +21,35 @@ function parse_extensions {
 			temp=`grep $ext $files | wc -l`
 			ExtCount[$ext]=$[ExtCount[$ext
 			] + $temp]
-			echo $ext ${ExtCount[$ext]}
+			#echo "Temp:$temp"
+			total=$[$total + ${ExtCount[$ext]}]
+			#echo $ext ${ExtCount[$ext]}
 			
 		done
 
-		echo ${files[*]}
-
-		#todo
-		return $index
 	fi
 
 	if [ "$1" = "fs2" ]; then
 		# If in fs2/, files 'extensions.txt' and 'extensions2.txt' contain file listings.
-		cd ../../fs2
+		cd fs2
 		pwd
 		for file  in `find . -type f -name "extensions.txt" -or -name "extensions2.txt"`; do
 			files[$index]=$file
 			index=$[$index + 1]
 		done
 
-		echo ${files[*]}
-
-
 		#todo
-		return 2
+		total=2
 	fi
+	cd $OLDPWD
+	echo $total
 }
 
 image_formats=("\.jpeg" "\.bmp" "\.png" "\.gif")
 source_formats=("\.c" "\.py" "\.pl" "\.sh")
 
-parse_extensions "fs1" image_formats[@]
-img_total=$?
-echo $img_total
+img_total=$(parse_extensions "fs1" image_formats[@])
+printf "\nTotal Image Files: $img_total \n"
 parse_extensions "fs2" source_formats[@]
 src_total=$?
 echo $src_total
