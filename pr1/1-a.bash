9bbc9ed68e26a34da50cc574927073250e410dd3
AdamVanScyoc <Avanscy@clemson.edu>
2018-09-08 14:21:56 -0400

    Refine parse extensions for fs1/extensions

--- pr1/1-a.bash
+++ pr1/1-a.bash
@@ -5,17 +5,36 @@ function parse_extensions {
 	# Check whether we are in fs1 or fs2
 	index=0
 	declare -a files
+	declare -a Exts
+	immediate=0
+	total=0
+	declare -A ExtCount
 
 	if [ "$1" = "fs1" ]; then
 		# If in fs1/, all files without an extension in extensions/ contain file listings.
 		cd fs1/extensions
 		pwd
 		for file  in `find . -type f ! -name "*.txt" ! -name "*.sh" ! -name "*.pl" ! -name "*.py" ! -name "*.bash"`; do
-			files[$index]=$file
-			index=$[$index + 1]
+			echo $file
+#			for ext in "$@"; do
+#				ExtCount[$ext]=0
+#			done
+			#todo: find out why this only works for .gif's
+			Exts=("${!2}")
+			for ext in "${Exts[@]}"; do
+				temp=`grep "$ext" "$file" | wc -l`
+				echo $ext $file
+				grep "$ext" "$file" | wc -l
+				ExtCount[$ext]=$[ExtCount[$ext] + $temp]
+				echo $ext ${ExtCount[$ext]}
+				
+			done
 		done
 
 		echo ${files[*]}
+
+		#todo
+		return $index
 	fi
 
 	if [ "$1" = "fs2" ]; then
@@ -28,8 +47,19 @@ function parse_extensions {
 		done
 
 		echo ${files[*]}
+
+
+		#todo
+		return 2
 	fi
 }
 
-parse_extensions "fs1"
-parse_extensions "fs2"
+image_formats=("\.jpeg", "\.bmp", "\.png", "\.gif")
+source_formats=("\.c", "\.py", "\.pl", "\.sh")
+
+parse_extensions "fs1" image_formats[@]
+img_total=$?
+echo $img_total
+parse_extensions "fs2" source_formats[@]
+src_total=$?
+echo $src_total
