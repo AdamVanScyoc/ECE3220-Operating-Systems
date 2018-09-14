81d943f04deeca067d76c4633d0329e3bd085d6a
AdamVanScyoc <Avanscy@clemson.edu>
2018-09-13 20:25:54 -0400

    Add metrics for < 1990, 1990 < x < 1995, and total

--- pr1/1-a.bash
+++ pr1/1-a.bash
@@ -6,16 +6,17 @@ function parse_extensions {
 	index=0
 	declare files
 	declare -n Exts=$2
+	specialCmd=$3
 	immediate=0
 	total=0
 	declare -A ExtCount
 
-	if [ "$1" = "fs1/extensions" ]; then
+	if [ "$1" = "fs1" ]; then
 		# If in fs1/, all files without an extension in extensions/ contain file listings.
-		files=$(find "$PWD" -type f ! -name "*.txt" ! -name "*.sh" ! -name "*.pl" ! -name "*.py" ! -name "*.bash" ! -name "*.swo" ! -name "*.swp" ! -name "*.zip" -printf "%P ")
+		files=$(find $PWD -maxdepth 1 -type f ! -name "*.txt" ! -name "*.sh" ! -name "*.pl" ! -name "*.py" ! -name "*.bash" ! -name "*.swo" ! -name "*.swp" ! -name "*.zip" -printf "%P ")
 
 		for ext in "${!Exts[@]}"; do
-			temp=`grep ${Exts[$ext]} $files | wc -l`
+			temp=`grep ${Exts[$ext]} $files | awk '$4' | wc -l`
 			((ExtCount[$ext] += $temp))
 			total=$[$total + ${ExtCount[$ext]}]
 		done
@@ -26,10 +27,10 @@ function parse_extensions {
 
 	if [ "$1" = "fs2" ]; then
 		# If in fs2/, files 'extensions.txt' and 'extensions2.txt' contain file listings.
-		files=$(find . -type f \( -name "extensions.txt" -or -name "extensions2.txt" \) -printf "%P ")
+		files=$(find $PWD -maxdepth 1 -type f \( -name "ls-redaction.txt" \) -printf "%P ")
 
-		for ext in ${!Exts[@]}; do
-			temp=`grep $ext $files | wc -l`
+		for ext in "${!Exts[@]}"; do
+			temp=`grep ${Exts["$ext"]} $files | awk '$4' | wc -l`
 			((ExtCount[$ext] += $temp))
 			total=$[$total + ${ExtCount[$ext]}]
 		done
@@ -40,56 +41,107 @@ function parse_extensions {
 	printf " %d " $total
 }
 
+#function print_time_period {
+#
+#	case "$1" in
+#		"")
+#			printf "Searching files from all time periods.\n"
+#		;;
+#	"-newermt 19900101")
+#			printf "Searching files created prior to 1990\n"
+#		;;
+#	"-newermt 19960101 -not -newermt 19900101")
+#			printf "Searching files from 1990-1995	\n"
+#		;;
+#	esac
+#}
+
 declare -A ImageFormatToRegexMap=( [.jpeg]="\.jpeg$" [.bmp]="\.bmp$" [.png]="\.png$" [.gif]="\.gif$" )
 declare -A SourceFormatToRegexMap=( [.c]="\.c$" [.py]="\.py$" [.pl]="\.pl$" [.sh]="\.sh$" )
-declare -a directories=("fs1/extensions" "fs2")
+declare -a directories=("fs1" "fs2")
+declare -a awk=("{ print }" "\$7 < 1995 && \$7 > 1990 { print }" "\$7 < 1990 { print }")
 
-# Iterate through list of directories.
-for cur in ${directories[@]}; do
-	cd $cur
 
-	if [ $cur = "fs1" ]; then
-		filesToSearch=`find "$PWD" -type f ! -name "*.txt" ! -name "*.sh" ! -name "*.pl" ! -name "*.py" ! -name "*.bash" ! -name "*.swo" ! -name "*.swp" ! -name "*.zip" -printf "%P "`
-	else
-		filesToSearch=`find . -type f \( -name "extensions.txt" -or -name "extensions2.txt" \) -printf "%P "`
-	fi
+# Iterate through the time periods - any, < 1990, and 1990 < spec < 1996,
+# that the files came from.
+for spec in "${awk[@]}"; do
 
-	# Count all image filetypes in ImageFormatToRegexMap
-	printf "\nSearching for these Image File formats: "; echo "${!ImageFormatToRegexMap[@]}";  printf "In Directory: $cur\n"
-	printf "\tSearching these fies:\t"; echo $filesToSearch; printf "\n"
-	img_totals=$(parse_extensions $cur ImageFormatToRegexMap)
-	declare -a totals
-	count=0
-	for i in $img_totals[@]; do
-		totals[count]=$i		
-		((count++))
-	done
+	# Iterate through list of directories.
+	for cur in ${directories[@]}; do
+		cd $cur
 
-	count=0
-	for k in ${!ImageFormatToRegexMap[@]}; do
-		#printf "\tFile Extension:\t%s\tCount: %d\n" $k $img_totals[$count]
-		printf "\tFile Extension: $k\tCount: ${totals[$count]}\n"
-		((count++))
-	done
-	printf "\tTotal Image Files: ${totals[4]}\n\n"
+		if [ $cur = "fs1" ]; then
+			filesToSearch=`find "$PWD" -maxdepth 1 -type f ! -name "*.txt" ! -name "*.sh" ! -name "*.pl" ! -name "*.py" ! -name "*.bash" ! -name "*.swo" ! -name "*.swp" ! -name "*.zip" -printf "%P "`
+		else
+			filesToSearch="extensions.txt"
+		fi
 
-	# Count all source code filetypes in SourceFormatToRegexMap
-	printf "Searching for these Source Code formats: "; echo "${!SourceFormatToRegexMap[@]}"; printf "In Directory: $cur\n"
-	printf "\tSearching these fies:\t"; echo $filesToSearch; printf "\n"
-	src_totals=$(parse_extensions $cur SourceFormatToRegexMap)
+		# Count all image filetypes in ImageFormatToRegexMap
+		printf "\nSearching for these Image File formats: "; echo "${!ImageFormatToRegexMap[@]}";  printf "In Directory: $cur\n"
+		printf "\tSearching these files:\t"; echo $filesToSearch; printf "\n"
+			
 
-	count=0
-	for i in $src_totals[@]; do
-		totals[count]=$i
-		((count++))
-	done
+		# Decide, based on what years we are searching for, how to call function and how to display search period.
+		case "$spec" in
+			"{ print }")
+				printf "Searching files from all time periods.\n"
+			;;
+		"\$7 < 1995 && \$7 > 1990 { print }'")
+				printf "Searching files created prior to 1990\n"
+			;;
+		"\$7 < 1990 { print }")
+				printf "Searching files from 1990-1995	\n"
+			;;
+		esac
 
-	count=0
-	for k in ${!SourceFormatToRegexMap[@]}; do
-		printf "\tFile Extension: $k\tCount: ${totals[$count]}\n"
-		((count++))
-	done
-	printf "\tTotal Source Code Files: ${totals[4]}\n"
-	
-	cd $OLDPWD
-done
+		img_totals=$(parse_extensions $cur ImageFormatToRegexMap "$spec")
+
+		declare -a totals
+		count=0
+		for i in $img_totals[@]; do
+			totals[count]=$i		
+			((count++))
+		done
+
+		count=0
+		for k in ${!ImageFormatToRegexMap[@]}; do
+			printf "\tFile Extension: $k\tCount: ${totals[$count]}\n"
+			((count++))
+		done
+		printf "\tTotal Image Files: ${totals[4]}\n\n"
+
+		# Count all source code filetypes in SourceFormatToRegexMap
+		printf "Searching for these Source Code formats: "; echo "${!SourceFormatToRegexMap[@]}"; printf "In Directory: $cur\n"
+		printf "Searching these files:\t"; echo $filesToSearch; printf "\n"
+		# Decide, based on what years we are searching for, how to call function and how to display search period.
+		case "$spec" in
+			"awk '{ print }'")
+				printf "Searching files from all time periods.\n"
+				img_totals=$(parse_extensions $cur ImageFormatToRegexMap $spec)
+			;;
+		"awk '\$7 < 1995 && \$7 > 1990 { print }'")
+				printf "Searching files created prior to 1990\n"
+			;;
+		"awk '\$7 < 1990 { print }")
+				printf "Searching files from 1990-1995	\n"
+			;;
+		esac
+
+		src_totals=$(parse_extensions $cur SourceFormatToRegexMap $spec)
+
+		count=0
+		for i in $src_totals[@]; do
+			totals[count]=$i
+			((count++))
+		done
+
+		count=0
+		for k in ${!SourceFormatToRegexMap[@]}; do
+			printf "\tFile Extension: $k\tCount: ${totals[$count]}\n"
+			((count++))
+		done
+		printf "\tTotal Source Code Files: ${totals[4]}\n"
+		
+		cd $OLDPWD
+	done # end for directories
+done # end for spcCmd
